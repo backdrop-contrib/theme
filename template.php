1a6f34ae52ffdd6f5172b73602e66b94b9fafa93
BWPanda <peter@panda.id.au>
2022-07-04 15:19:33 +1000

    Merge code from backdrop-ops repo.

--- template.php
+++ template.php
@@ -1,45 +1,33 @@
 <?php
 /**
  * @file
- * Preprocess, theme, and alter hooks.
+ * MyTheme preprocess functions and theme function overrides.
  */
-
-/*******************************************************************************
- * Preprocess functions here prepare variables for existing templates.
-*******************************************************************************/
 
 /**
  * Prepares variables for node templates.
  *
  * @see node.tpl.php
  */
-function template_preprocess_node(&$variables) {
+function mytheme_preprocess_node(&$variables) {
   $variables['classes'][] = 'content';
 }
-
-/*******************************************************************************
- * Theme functions here override existing theme functions.
-*******************************************************************************/
 
 /**
  * Overrides theme_breadcrumb().
  */
-function foo_breadcrumb($variables) {
+function mytheme_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
   $output = '';
+
   if (!empty($breadcrumb)) {
     // Add useful aria-label for screen-readers.
     $output .= '<nav role="navigation" class="breadcrumb" aria-label="Website Orientation">';
-    // Remove confusing you-are-here heading for screen-readers.
+
+    // Remove core's confusing you-are-here heading for screen-readers.
     $output .= '<ol><li>' . implode(' » </li><li>', $breadcrumb) . '</li></ol>';
     $output .= '</nav>';
   }
+
   return $output;
 }
-
-/*******************************************************************************
- * Alter functions here will alter existing data structures.
-*******************************************************************************/
-
-/* Note: using alter functions in a theme is not a best practice. It is safer
-   to alter data structires from a module whenever possible. */
